ea00755ebe7306533cdc54d62104a9ff044da50b
Nikita <nikitya39@gmail.com>
2022-03-10 11:18:47 +0200

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int b = 1;
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+while (b <= a)
+{
+    Console.WriteLine((b * b * b));
+    b++;
+}
+          
+Console.ReadKey();
