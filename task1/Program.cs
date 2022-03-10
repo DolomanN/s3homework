9e85b30edd4b49379beb8520c41ea67531b6f274
Nikita <nikitya39@gmail.com>
2022-03-10 11:18:26 +0200

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Convert.ToString(Console.ReadLine());
+char[] s = number.ToCharArray();
+Array.Reverse(s);
+if (Convert.ToString(s) == number)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
